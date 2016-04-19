18fb463cbb13085ede125f47395cac847993b89f
Elliot Harris <elliotharris@live.co.uk>
2016-04-19 15:09:28 +0100

    Removed useless using

--- CardGameServer/CardGameTestUI/Properties/Resources.Designer.cs
+++ CardGameServer/CardGameTestUI/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CardGameTestUI.Properties {
-    using System;
-    
-    
+namespace CardGameTestUI.Properties
+{
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
